df3c0abb3002bb9ce41decc0d047095400429a28
Thomas Alexander Ewald <thomas@humantech.com.br>
2017-05-25 13:58:14 -0300

    Added support for RelatedOnlyFieldListFilter based dropdown filter

--- django_admin_listfilter_dropdown/filters.py
+++ django_admin_listfilter_dropdown/filters.py
@@ -1,4 +1,5 @@
 from django.contrib.admin.filters import AllValuesFieldListFilter, RelatedFieldListFilter, ChoicesFieldListFilter
+from django.contrib.admin import RelatedOnlyFieldListFilter
 
 class DropdownFilter(AllValuesFieldListFilter):
     template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
@@ -7,4 +8,7 @@ class ChoiceDropdownFilter(ChoicesFieldListFilter):
     template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
 
 class RelatedDropdownFilter(RelatedFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
+
+class RelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
     template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
